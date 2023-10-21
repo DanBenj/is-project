2696d09f32e9653936a4151c247a11ce8f82cd0b
DanBenj <92568377+DanBenj@users.noreply.github.com>
2023-10-21 12:10:49 +0000

    initial setup

---
+++ my-app/my-app/src/App.js
@@ -0,0 +1,16 @@
+import { Admin, Resource } from "../../../js-sdk";
+import { PocketBaseProvider } from "../../../ra-pocketbase";
+import { TodoCreate, TodoEdit, TodoList, TodoShow } from "./todos";
+
+const pbProvider = PocketBaseProvider('http://127.0.0.1:8090/');
+
+function App() {
+  return (
+    <Admin dataProvider={pbProvider.dataProvider} authProvider={pbProvider.authProvider}>
+      <Resource name="todos" list={TodoList} show={TodoShow} create={TodoCreate} edit={TodoEdit} recordRepresentation="task" />
+    </Admin>
+  )
+}
+
+export default App
+  
